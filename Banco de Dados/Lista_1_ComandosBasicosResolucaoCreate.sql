4a370c054842c8a003b54025f492858d6f89f04c
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-03-29 16:58:00 -0300

    Lista01_BD

--- Banco de Dados/Lista_1_ComandosBasicosResolucaoCreate.sql
+++ Banco de Dados/Lista_1_ComandosBasicosResolucaoCreate.sql
@@ -18,8 +18,7 @@ CREATE TABLE Cidade
     Nome NVARCHAR(45),
     UF CHAR(2),
     PaisID INT,
-    PRIMARY KEY (CidadeID),
-    FOREIGN KEY (PaisID) REFERENCES Pais(PaisID)
+    PRIMARY KEY (CidadeID)
 );
 
 CREATE TABLE Tipo
@@ -34,7 +33,7 @@ CREATE TABLE Cliente
 (
     ClienteID INT NOT NULL,
     Nome NVARCHAR(45),
-    TipoID INT,
+    Tipo NVARCHAR(20),
     Contato NVARCHAR(45),
     Cargo NVARCHAR(45),
     Endereco NVARCHAR(90),
@@ -43,9 +42,7 @@ CREATE TABLE Cliente
     Fone NVARCHAR(45),
     Fax NVARCHAR(45),
     Obs NVARCHAR(45),
-    PRIMARY KEY (ClienteID),
-    FOREIGN KEY (CidadeID) REFERENCES Cidade(CidadeID),
-    FOREIGN KEY (TipoID) REFERENCES Tipo(TipoID)
+    PRIMARY KEY (ClienteID)    
 );
 
 CREATE TABLE Produto
@@ -60,15 +57,14 @@ CREATE TABLE Produto
     Estmin INT,
     TipoID INT,
     Situacao NVARCHAR(45),
-    PRIMARY KEY (ProdutoID),
-    FOREIGN key (TipoID) REFERENCES Tipo(TipoID)
+    PRIMARY KEY (ProdutoID)    
 );
 
 CREATE TABLE Funcao
 (
     FuncaoID INT NOT NULL,
     Nome NVARCHAR(45),
-    Gratific decimal/*Duvida do Tipo*/,
+    Gratific decimal,
     PRIMARY KEY (FuncaoID)
 );
 
@@ -77,7 +73,7 @@ CREATE TABLE Setor
     SetorID INT NOT NULL,
     Nome NVARCHAR(45),
     Ramal NVARCHAR(45),
-    Chefe NVARCHAR(45),
+    Chefe INT NOT NULL,
     PRIMARY KEY (SetorID)
 );
 
@@ -87,10 +83,10 @@ CREATE TABLE Funcionario
     Nome NVARCHAR(45),
     Sexo NVARCHAR(45),
     Estcivil NVARCHAR(45),
-    RG NVARCHAR(45),
-    CPF NVARCHAR(45),
+    RG NVARCHAR(50),
+    CPF NVARCHAR(10),
     Datanasc DATETIME,
-    Naturalidade NVARCHAR(45),
+    Naturalidade NVARCHAR(6),
     Dataadm DATETIME,
     Endereco NVARCHAR(45),
     Complemento NVARCHAR(45),
@@ -104,10 +100,7 @@ CREATE TABLE Funcionario
     Salario DECIMAL,
     Email NVARCHAR(45),
     Obs NVARCHAR(45),
-    PRIMARY KEY (FuncionarioID),
-    FOREIGN KEY (CidadeID) REFERENCES Cidade(CidadeID),
-    FOREIGN KEY (FuncaoID) REFERENCES Funcao(FuncaoID),
-    FOREIGN KEY (SetorID) REFERENCES Setor(SetorID)
+    PRIMARY KEY (FuncionarioID)        
 );
 
 CREATE TABLE Pedido
@@ -118,21 +111,16 @@ CREATE TABLE Pedido
     DataPedido DATETIME,
     DataFatura DATETIME,
     Via NVARCHAR(45),
-    Frete NVARCHAR(45),/*Duvida Tipo*/
-    PRIMARY KEY (PedidoID),
-    FOREIGN KEY (ClienteID) REFERENCES Cliente(ClienteID),
-    FOREIGN KEY (Vendedor) REFERENCES Funcionario(FuncionarioID)
+    Frete NVARCHAR(45),
+    PRIMARY KEY (PedidoID)   
 );
 
 CREATE TABLE Itens
 (
-    -- ItensID INT NOT NULL,
     PedidoID INT NOT NULL,
     ProdutoID INT NOT NULL,
     Preco decimal,
     Quant INT,
     Desconto DECIMAL,
-    PRIMARY KEY (PedidoID),
-    -- FOREIGN KEY (PedidoID) REFERENCES Pedido(PedidoID),
-    FOREIGN KEY (ProdutoID) REFERENCES Produto(ProdutoID)
+    PRIMARY KEY (PedidoID, ProdutoID)    
 );
--- Banco de Dados/Lista_1_ComandosBasicosResolucaoInsert.sql
+++ Banco de Dados/Lista_1_ComandosBasicosResolucaoInsert.sql
@@ -1,48 +1,54 @@
 INSERT INTO Pais
     (PaisID, Sigla, Nome)
 VALUES
-    (1, 'Brasil', 'BRA'),
-    (2, 'Alemanha', 'GER'),
-    (3, 'Estados Unidos', 'USA');
+    (1, "BRA", "Brasil"),
+    (2, "USA", "Estados Unidos"),
+    (3, "PER", "Peru"),
+    (4, "JAP", "Japao"),
+    (5, "POR", "Portugal"),
+    (6, "RUS", "Russia");
 
 INSERT INTO Cidade
     (CidadeId, Nome, UF, PaisID)
 VALUES
-    (3549805, 'São José do Rio Preto', 'SP', 1),
-    (3529005, 'Marília', 'SP', 1),
-    (4558222, 'Berlim', 'BE', 2),
-    (4522598, 'Munique', 'BY', 2),
-    (5533258, 'Nova York', 'NY', 3),
-    (5525987, 'Orlando', 'FL', 3);
+    (1, "Marilia", "SP", 1),
+    (2, "Michigan", "IL", 2),
+    (3, "Sydney", "Nova Gales do Sul", 6),
+    (4, "Nagoia", "AIC", 5),
+    (5, "Berlin", "Berlin", 4),
+    (6, "Cordoba", "Cordoba", 3);
 
 INSERT INTO Tipo
     (TipoID, Nome, Descricao)
 VALUES
-    (1, 'Desinfetantes', 'Os produtos deste grupo são produtos biocidas utilizados na higiene.'),
-    (2, 'Alimentos', 'Produtos destinados a alimentação.'),
-    (3, 'Celulares', 'Produtos para comunicação por ondas electromagneticas.'),
-    (4, 'Eletrodomesticos', 'Produtos eletricos para facilitar varias tarefas domesticas.'),
-    (5, 'Fisica', 'Clientes que possuem cadastro de pessoa fisica.'),
-    (6, 'Juridica', 'Clientes que possuem cadastro de pessoa juridica');
+    (1, "TIPO 1", "DESCRICAO TIPO 1"),
+    (2, "TIPO 2", "DESCRICAO TIPO 2"),
+    (3, "TIPO 3", "DESCRICAO TIPO 3"),
+    (4, "TIPO 4", "DESCRICAO TIPO 4");
 
 INSERT INTO Cliente
-    (ClienteID, Nome, TipoID, Contato, Cargo, Endereco, CidadeID, CEP,Fone, Fax, Obs)
+    (ClienteID, Nome, Tipo, Contato, Cargo, Endereco, CidadeID, CEP,Fone, Fax, Obs)
 VALUES
-    (1, 'Alexandra Ferreira Da Silva', 5, 'alaexandre@gmail.com', 'Policial', 'R. Roberto Simonsen', 3549805, '	15090-120', '(17)3216-9400', '', ''),
-    (2, 'Debora Ribeiro Santos', 5, 'debora10@gmail.com', 'CEO', 'Av. José Munia', 3549805, '15085-350', '(17)2137-3570', '', ''),
-    (3, 'Rodrigo Garcia Cardoso', 5, 'rodrigo.cardos97@icloud.com', 'R. Sete de Setembro 1440', 3529005, '17502-020', '(14)3401-4400', '', ''),
-    (4, 'Dylan Smith', 5, 'dylan@gmail.com', '99 West South St', 5525987 , 'FL 32801', '+1 855-724-5411', '', ''),
-    (5, 'Arnold Ross', 5, 'arnold@google.com', '768 5th Ave', 5533258, 'NY 10019', '+1 212-759-3000', '', ''),
-    (6, 'Anton Barth', 5, 'a.barth@icloud.com', 'Karl-Marx-Allee 33', 4558222, '10178 Berlin', '+49 30 24756011', '', ''),
-    (7, 'Stephan Beck', 5, 'ste.beck@gmail.com', 'Zamdorfer Str. 84', 4522598, '81677 München', '+49 89 9394430', '', '');
+    (1, "Luiz Jose", " ", " ", "vendedor", "Av Faria lima 23", 1, "15214500", "551133012598", null, null),
+    (2, "Rosemary"," ", " ", "marketing", "20 Saint James Av", 2, "20058", "16233022015", null, null),
+    (3, "Janet", " ", " ", "teacher", "550 Ocean Av", 3, "20058", "16233022015", "16256985632", "blablabla"),
+    (4, "Cristian pela", " ", " ", "estudante", "Rua de la plata 68", 6, "6686464", "54153265546", null, null),
+    (5, "Cristina Rode", " ", " ", "programadora", "Frederich Strasse 125", 5, "526548", "3546544", null, null),
+    (6, "Liu xiang", " ", " ", "professora", "feng xang da lu 12", 4, "35465465", "84213546546", null, "peng you");
 
 INSERT INTO Produto
-    (ProdutoID, Nome, Descricao, Apresent, Venda, Custo, Quantest, Estmin, TipoID, Situacao)
+    (ProdutoID, Nome, TipoID, Descricao, Apresent, Venda, Custo, Quantest, Estmin, Situacao)
 VALUES
-    (1, 'Omo', 'Sabao para roupas', '', 15.95, 7.35, 500, 100, 1, 'Normal'),
-    (2, 'Pit-Stop', 'Bolacha de agua e sal', '', 2.30, 0.3, 1000, 110, 2, 'Normal'),
-    (3, 'iPhone 6s 128gb', 'O melhor Smartphone', '', 2599, 1250.56, 500, 2, 3, 'Normal'),
-    (4, 'Geladeira', 'Samsung', '', 5859, 65, 2400, 8, 1, 4, 'Normal');
+    (1, "PRODUTO 1", 1, "DESCRICAO PRODUTO 1", "APRESENT PRODUTO 1", 125.25, 78.90, 150, 20, "situacao 1" ),
+    (2, "PRODUTO 2", 2, "DESCRICAO PRODUTO 2", "APRESENT PRODUTO 1", 125.25, 78.90, 110, 20, "situacao 2" ),
+    (3, "PRODUTO 3", 3, "DESCRICAO PRODUTO 3", "APRESENT PRODUTO 1", 125.25, 78.90, 85, 20, "situacao 3" ),
+    (4, "PRODUTO 4", 4, "DESCRICAO PRODUTO 4", "APRESENT PRODUTO 1", 125.25, 78.90, 78, 30, "situacao 4" ),
+    (5, "PRODUTO 5", 1, "DESCRICAO PRODUTO 5", "APRESENT PRODUTO 1", 125.25, 78.90, 98, 20, "situacao 5" ),
+    (6, "PRODUTO 6", 2, "DESCRICAO PRODUTO 6", "APRESENT PRODUTO 1", 125.25, 78.90, 101, 50, "situacao 6" ),
+    (7, "PRODUTO 7", 4, "DESCRICAO PRODUTO 7", "APRESENT PRODUTO 1", 125.25, 78.90, 58, 50, "situacao 7" ),
+    (8, "PRODUTO 8", 1, "DESCRICAO PRODUTO 8", "APRESENT PRODUTO 1", 125.25, 78.90, 145, 10, "situacao 8" ),
+    (9, "PRODUTO 9", 2, "DESCRICAO PRODUTO 9", "APRESENT PRODUTO 1", 125.25, 78.90, 189, 80, "situacao 9" ),
+    (10, "PRODUTO 10", 3, "DESCRICAO PRODUTO 10", "APRESENT PRODUTO 1", 125.25, 78.90, 112, 30, "situacao 10" );
 
 INSERT INTO Funcao
     (FuncaoID, Nome, Gratific)
@@ -54,13 +60,35 @@ VALUES
 INSERT INTO Setor
     (SetorID, Nome, Ramal, Chefe)
 VALUES
-    (1, 'Administrativo', '1001', 'Rafael Lucas'),
-    (2, 'Financeiro', '1012', 'Eduardo'),
-    (3, 'Comercial', '1039', 'Rafael Lucas'),
-    (4, 'Operacional', '1015', 'Luan Dumas'),
-    (5, 'Recursos Humanos', '1000', 'Roberto Lucas');
+    (1, "SETOR 1", "1221", 4),
+    (2, "SETOR 2", "1224", 2),
+    (3, "SETOR 3", "1223", 3),
+    (4, "SETOR 4", "1222", 4);
 
-INSERT INTO Setor
+INSERT INTO Funcionario
     (FuncionarioID, Nome, Sexo, Estcivil, RG, CPF, Datanasc, Naturalidade, Dataadm, Endereco, Complemento, Bairro, CidadeID, CEP, Fone, Celular, FuncaoID, SetorID, Salario, Email, Obs)
 VALUES
-    (1,'Alex Silva Araujo', 'Masculino', 'Solteiro', '25.253.394-X', '213.909.848-00', '01/02/1985', 'R. Das Flores 415', '12345-456', '(19)1234-4567', '(19)99123-4567', 3, 3, 3500.95, 'alex@uol.com.br', ''),
+    (1, "Josefina Almeida", "F", "Solteira", "485221145", "11155588874", "1992-05-20", 1, "2015-05-21", "Rua jose bras 21", null, "Parque das Orquideas", 1, "215452", "112246654654", "5464654654", 2, 3, 1.200, "nome@email.com.br", null ),
+    (2, "Mauro benedito", "M", "Casado", "485465145", "1145454874", "1980-09-10", 1, "2014-01-21", "Rua jose bras 22", null, "Parque das Orquideas", 1, "215452", "112265874654", "546545654654", 2, 4, 1.500, "nome2@email.com.br", null ),
+    (7, "Mauro benedito", "M", "Casado", "485465145", "1145454874", "1980-09-10", 1, "2014-01-21", "Rua jose bras 22", null, "Parque das Orquideas", 1, "215452", "112265874654", "546545654654", 2, 4, 1.500, "nome2@email.com.br", null ),
+    (6, "Camila Silva", "F", "Solteira", "485221145", "111555884874", "1986-02-12", 3, "2013-02-21", "Rua jose bras 23", null, "Palmital", 6, "215452", "112246654654", "5464654654", 2, 1, 3.200, "nome3@email.com.br", null ),
+    (4, "John Feer", "M", "Solteira", "485221145", "11155588874", "1990-10-04", 2, "2012-03-21", "Rua jose bras 24", null, "Cezar Almeida", 2, "215452", "112246654654", "5464654654", 3, 2, 5.200, "nome4@email.com.br", null ),
+    (5, "Michael Flanagam", "M", "Solteira", "485221145", "11155588874", "1978-08-29", 4, "2011-04-21", "Rua jose bras 25", null, "Parque das Nações", 5, "215452", "112246654654", "5464654654", 1, 3, 2.200, "nome5@email.com.br", null ),
+    (3, "Kena Fany", "F", "Solteira", "485221145", "11155588874", "1995-03-21", 5, "2010-09-21", "Rua jose bras 26", null, "Parque das Nações", 4, "215452", "112246654654", "5464654654", 2, 3, 4.100, "nome6@email.com.br", null );
+
+INSERT INTO PEDIDO
+    (PedidoID, ClienteID, Vendedor, DataPedido, DataFatura, Via, Frete)
+VALUES
+    (1, 2, 1, "2017-20-01", "2017-22-01", "VIA 1", "FRETE1"),
+    (2, 1, 2, "2017-25-01", "2017-28-01", "VIA 1", "FRETE1"),
+    (3, 3, 3, "2017-10-02", "2017-15-02", "VIA 1", "FRETE1"),
+    (4, 4, 1, "2017-18-02", "2017-22-02", "VIA 1", "FRETE1");
+
+INSERT INTO ITENS
+    (PedidoID, ProdutoID, Preco, Quant, Desconto)
+VALUES
+    (1, 2, 152.20, 2, 20.0),
+    (1, 4, 182.20, 2, 20.0),
+    (2, 1, 112.20, 1, 12.0),
+    (3, 3, 152.20, 3, 7.0),
+    (4, 4, 182.20, 1, 5.0);
