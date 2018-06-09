ea85d836bc921a81fc39bc20aaea5e85fab34692
Rodrigo <rodrigo_cardoso97@hotmail.com>
2018-06-09 17:16:46 -0300

    marca form

--- ProjetoN2/Estoque/ClientApp/app/components/marca-form/marca-form.component.ts
+++ ProjetoN2/Estoque/ClientApp/app/components/marca-form/marca-form.component.ts
@@ -1,5 +1,6 @@
 import { marca } from './../../models/marca';
 import { Component, OnInit } from '@angular/core';
+import { MarcaService } from '../../services/marca.service';
 
 @Component({
   selector: 'app-marca-form',
@@ -7,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marca-form.component.css']
 })
 export class MarcaFormComponent implements OnInit {
-  marca: marca = {
-    id: 0,
-    nome: ''
-  };
+  marca: any[] = [];
 
-  constructor() { }
+  constructor(private marcaService: MarcaService) { }
 
   ngOnInit() {
+    this.marcaService.getMarcas().subscribe(marca => {
+      this.marca = marca
+      console.log("MARCAS", this.marca);
+    });
   }
-
 }
